aaa9bb6d5d6a5b7396bf6031415553e975049793
nate <nathan@trvc.net>
2024-03-09 11:35:43 +0000

    v2 gloves images
    v2 gloves images and node js script

---
+++ import-json-gloves.js
@@ -0,0 +1,41 @@
+const fs = require('fs'); // Require the 'fs' module for file operations
+const gloves = require('./gloves.json');
+const apiHubRes = require('./cs2market.json');
+
+function extractWear(name) {
+  const wear = name.match(
+    /(Factory New|Minimal Wear|Field-Tested|Well-Worn|Battle-Scarred)/,
+  );
+  return wear ? wear[0] : null;
+}
+
+function findImagesFor(name) {
+  return apiHubRes.data
+    .filter((item) => {
+      return item.marketHashName.includes(name);
+    })
+    .map((item) => {
+      return {
+        [extractWear(item.marketHashName)]: item.image,
+      };
+    })
+    .reduce((acc, curr) => {
+      return { ...acc, ...curr };
+    });
+}
+
+const res = gloves.map((gloves) => {
+  return {
+    ...gloves,
+    image: findImagesFor(gloves.name),
+  };
+});
+
+// Write the result to a new JSON file
+fs.writeFile('result.json', JSON.stringify(res, null, 2), (err) => {
+  if (err) {
+    console.error('Error writing JSON file:', err);
+  } else {
+    console.log('JSON file has been saved as result.json');
+  }
+});
