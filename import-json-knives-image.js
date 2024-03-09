7a99cf578b70c9acac1e0750c37fef89185f1a69
nate <nathan@trvc.net>
2024-03-09 11:29:28 +0000

    v2 knives images
    minimal wear link moved into factory new link also to replace old image

---
+++ import-json-knives-image.js
@@ -0,0 +1,46 @@
+const fs = require('fs'); // Require the 'fs' module for file operations
+const knives = require('./knives.json');
+const apiHubRes = require('./cs2market.json');
+
+function extractWear(name) {
+  const wear = name.match(
+    /(Factory New|Minimal Wear|Field-Tested|Well-Worn|Battle-Scarred)/,
+  );
+  return wear ? wear[0] : null;
+}
+
+function findImagesFor(name) {
+  return apiHubRes.data
+    .filter((item) => {
+      return item.marketHashName.includes(name);
+    })
+    .map((item) => {
+      return {
+        [extractWear(item.marketHashName)]: item.image,
+      };
+    })
+    .reduce((acc, curr) => {
+      return { ...acc, ...curr };
+    });
+}
+
+const res = knives.map((knife) => {
+  const images = findImagesFor(knife.name);
+  // Replace Factory New image with Minimal Wear image
+  if (images['Minimal Wear'] && images['Factory New']) {
+    images['Factory New'] = images['Minimal Wear'];
+  }
+  return {
+    ...knife,
+    image: images,
+  };
+});
+
+// Write the result to a new JSON file
+fs.writeFile('result.json', JSON.stringify(res, null, 2), (err) => {
+  if (err) {
+    console.error('Error writing JSON file:', err);
+  } else {
+    console.log('JSON file has been saved as result.json');
+  }
+});
